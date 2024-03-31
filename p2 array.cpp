cda8c62f75cce87472a237eb1faf7b5ff166ce38
rendis02 <165538770+rendis02@users.noreply.github.com>
2024-03-31 12:15:59 +0700

    Update p2 array.cpp

--- p2 array.cpp
+++ p2 array.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
 
+int main() {
+    int angka[5];
+    int *p;
+
+    p = angka;
+    *p + 10;
+    p++;
+    *p = 20;
+    p = &angka[2];
+    *p = 30;
+    p = angka + 3;
+    *p = 40;
+    p = angka;
+    *(p+4) = 50;
+
+    for (int i = 0; i < 5;i++){
+        cout << angka << ",";
+    }
+    cout << angka << endl;
+
+    return 0;
+}

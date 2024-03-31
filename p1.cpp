f3d94417bb8d7bf9f80b5d967d124e3c617c2b43
rendis02 <165538770+rendis02@users.noreply.github.com>
2024-03-31 12:08:33 +0700

    Update p1.cpp

--- p1.cpp
+++ p1.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int main()
+{
+    cout << ">>> Menulis ke File" << endl;
+    ofstream file_keluaran;
+    file_keluaran.open("latihan.txt");
+
+    cout << "Sedang Merekam... " << endl;
+    file_keluaran << "Haloo " << endl;
+    file_keluaran << "INI ADALAH SAYA" << endl;
+    file_keluaran << "JAMAL" << endl;
+    file_keluaran.close();
+
+    cout << ">>> Membaca dari File" << endl;
+
+    string line;
+    ifstream file_input;
+    file_input.open("latihan.txt");
+
+    cout << "Membaca isi file latihan.txt" << endl;
+    while(file_input)
+    {
+        getline(file_input, line);
+        cout << line << endl;
+    }
+    
+}
